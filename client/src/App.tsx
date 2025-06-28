8bc22ec33673360345077c394f9503730f20d45b
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2025-06-28 12:41:17 +0300

    update: theme-initalizer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { useEffect } from 'react';
-
 import { AppRoutes } from './app-routes';
 import { AuthChecker } from './features/auth/components';
-import { AlertsContainer } from './shared/components';
-import { useTheme } from './shared/hooks';
+import { AlertsContainer, ThemeInitializer } from './shared/components';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,16 +13,12 @@ const queryClient = new QueryClient({
 });
 
 function App() {
-  const { setCurrentTheme } = useTheme();
-
-  useEffect(() => {
-    setCurrentTheme();
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <AuthChecker>
-        <AppRoutes />
+        <ThemeInitializer>
+          <AppRoutes />
+        </ThemeInitializer>
       </AuthChecker>
       <AlertsContainer />
     </QueryClientProvider>
--- client/src/shared/components/index.ts
+++ client/src/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './logo';
 export * from './alerts-container';
 export * from './users-list';
 export * from './theme-switcher';
+export * from './theme-initializer';
