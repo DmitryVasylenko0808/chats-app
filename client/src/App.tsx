ec2031694e6352472142a45422f817d15d5d7d67
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2025-06-28 12:35:33 +0300

    update: app-routes component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { useEffect } from 'react';
-import { Route, Routes, useLocation } from 'react-router';
 
-import { AuthChecker, RequireAuth } from './features/auth/components';
-import AuthLayout from './layouts/auth-layout';
-import BaseLayout from './layouts/base-layout';
-import BookmarksPage from './pages/bookmarks-page';
-import ChatPage from './pages/chat-page';
-import HomePage from './pages/home-page';
-import ProfileModal from './pages/modals/profile-modal';
-import SettingsModal from './pages/modals/settings-modal';
-import NotificationsPage from './pages/notifications-page';
-import ProfilePage from './pages/profile-page';
-import RegisterPage from './pages/register-page';
-import SettingsPage from './pages/settings-page';
-import SignInPage from './pages/sign-in-page';
+import { AppRoutes } from './app-routes';
+import { AuthChecker } from './features/auth/components';
 import { AlertsContainer } from './shared/components';
 import { useTheme } from './shared/hooks';
 
@@ -29,39 +17,15 @@ const queryClient = new QueryClient({
 
 function App() {
   const { setCurrentTheme } = useTheme();
-  const location = useLocation();
 
   useEffect(() => {
     setCurrentTheme();
   }, []);
 
-  const state = location.state as { backgroundLocation?: Location };
-
   return (
     <QueryClientProvider client={queryClient}>
       <AuthChecker>
-        <Routes location={state?.backgroundLocation || location}>
-          <Route element={<RequireAuth />}>
-            <Route path="/" element={<BaseLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path="/chats/:id" element={<ChatPage />} />
-              <Route path="/profile/:id" element={<ProfilePage />} />
-              <Route path="/bookmarks" element={<BookmarksPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
-              <Route path="/notifications" element={<NotificationsPage />} />
-            </Route>
-          </Route>
-          <Route path="auth" element={<AuthLayout />}>
-            <Route path="register" element={<RegisterPage />} />
-            <Route path="sign-in" element={<SignInPage />} />
-          </Route>
-        </Routes>
-        {state?.backgroundLocation && (
-          <Routes>
-            <Route path="/profile/:id" element={<ProfileModal />} />
-            <Route path="/settings" element={<SettingsModal />} />
-          </Routes>
-        )}
+        <AppRoutes />
       </AuthChecker>
       <AlertsContainer />
     </QueryClientProvider>
---
+++ client/src/app-routes.tsx
@@ -0,0 +1,48 @@
+import { Route, Routes, useLocation } from 'react-router';
+
+import { RequireAuth } from './features/auth/components';
+import AuthLayout from './layouts/auth-layout';
+import BaseLayout from './layouts/base-layout';
+import BookmarksPage from './pages/bookmarks-page';
+import ChatPage from './pages/chat-page';
+import HomePage from './pages/home-page';
+import ProfileModal from './pages/modals/profile-modal';
+import SettingsModal from './pages/modals/settings-modal';
+import NotificationsPage from './pages/notifications-page';
+import ProfilePage from './pages/profile-page';
+import RegisterPage from './pages/register-page';
+import SettingsPage from './pages/settings-page';
+import SignInPage from './pages/sign-in-page';
+
+export const AppRoutes = () => {
+  const location = useLocation();
+
+  const state = location.state as { backgroundLocation?: Location };
+
+  return (
+    <>
+      <Routes location={state?.backgroundLocation || location}>
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<BaseLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/chats/:id" element={<ChatPage />} />
+            <Route path="/profile/:id" element={<ProfilePage />} />
+            <Route path="/bookmarks" element={<BookmarksPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/notifications" element={<NotificationsPage />} />
+          </Route>
+        </Route>
+        <Route path="auth" element={<AuthLayout />}>
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="sign-in" element={<SignInPage />} />
+        </Route>
+      </Routes>
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route path="/profile/:id" element={<ProfileModal />} />
+          <Route path="/settings" element={<SettingsModal />} />
+        </Routes>
+      )}
+    </>
+  );
+};
